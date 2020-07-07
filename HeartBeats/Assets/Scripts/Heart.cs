68d83d8e429e9cb1c5531c5e4948a1199b81f377
orh92 <44766935+orh92@users.noreply.github.com>
2020-07-07 19:32:30 +0300

    add

---
+++ HeartBeats/Assets/Scripts/Heart.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+public class Heart : MonoBehaviour
+{
+
+
+    float a =10F;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {if (a ==9F) { a =10F;
+            Thread.Sleep(800);
+        }
+        else { a = 9F;
+            Thread.Sleep(250);
+        }
+        
+        transform.localScale = new Vector3(a, a, a);
+        
+       
+       
+    }
+}
---
+++ Rotator/Assets/Scripts/Rotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    float speed = 0.4F;
+  //Assign a GameObject in the Inspector to rotate around
+    public GameObject target;
+
+    void Update()
+    {
+        
+//transform.Rotate(Vector3.up * speed);
+           transform.Rotate(Vector3.forward * speed);
+      //  transform.Rotate(Vector3.right * speed);
+
+    }
+}
